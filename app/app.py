a32fc9660b5dc04fb8ef912d7a9830a561338d3f
Julia Szulc <me@juliaszulc.com>
2022-10-08 22:39:18 +0200

    Add monthly categories average

--- app/app.py
+++ app/app.py
@@ -2,13 +2,13 @@
 import streamlit as st
 
 import chart
-import helper
+import data
 
 st.set_page_config(page_title="Personal finances overview", page_icon="📊")
 
 st.title("Personal finances overview")
 
-history_df = helper.load_data()
+history_df = data.load_data()
 f"{len(history_df)} entries"
 f"from {history_df['date'].min().date()} to {history_df['date'].max().date()}"
 
@@ -18,7 +18,7 @@ st.header("Absolute balance")
 balance_resolution = st.radio(
     "Resolution:", ("days", "months"), horizontal=True, label_visibility="collapsed"
 )
-balance_df = helper.get_daily_balance(history_df)
+balance_df = data.get_daily_balance(history_df)
 
 if balance_resolution == "days":
     st.altair_chart(chart.create_balance_chart(balance_df), use_container_width=True)
@@ -30,5 +30,16 @@ else:
 
 st.header("Incomes vs. expenses")
 st.altair_chart(
-    chart.create_expenses_incomes_chart(helper.get_monthly_incomes_expenses(history_df))
+    chart.create_expenses_incomes_chart(data.get_monthly_incomes_expenses(history_df))
+)
+
+
+st.header("Monthly average per category")
+n_months = st.slider(
+    "Based on last n months:", 1, len(history_df["date"].dt.to_period("M").unique()), 12
+)
+st.dataframe(
+    data.get_mean_categories(history_df, n_months=n_months),
+    width=400,
+    height=570,
 )
